97ef0bd55b0b693f84cc1247098b4030c5357f10
PaulVoeth <paul.voeth@stud.hs-bochum.de>
2024-11-24 18:51:26 +0100

    backend eingefügt

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+let datensaetze = [];
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.json(datensaetze);
+});
+
+app.post("/", (req, res) => {
+  const date = new Date();
+  const customTimestamp = `${date.getFullYear()}-${
+    date.getMonth() + 1
+  }-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  const receivedData = req.body;
+
+  datensaetze.push(receivedData);
+
+  console.log(customTimestamp);
+  console.log(receivedData, "\n\n");
+  res.status(200).send("Daten erhalten");
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server läuft auf http://localhost:${PORT}`);
+});
